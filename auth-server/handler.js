d4a99268e338b0139fff1b9c87b268244f8dd614
Orlando-Adrian Calciu <orlandoadrian5444@outlook.com>
2022-08-18 11:58:52 +0100

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -13,7 +13,7 @@ const credentials = {
     token_uri: "https://oauth2.googleapis.com/token",
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
     redirect_uris: ["https://orlandoadriancalciu.github.io/meet-app/"],
-    javascript_origins: ["https://orlandoadriancalciu.github.io", "http://localhost:3000"],
+    javascript_origins: ["https://orlandoadriancalciu.github.io", "http://localhost:3000"]
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
@@ -31,7 +31,7 @@ module.exports.getAuthURL = async () => {
     return {
         statusCode: 200,
         headers: {
-            "Access-Control-Allow-Origin": "*"
+            "Access-Control-Allow-Origin": "*",
         },
         body: JSON.stringify({
             authUrl: authUrl,
@@ -59,7 +59,7 @@ module.exports.getAccessToken = async (event) => {
             return {
                 statusCode: 200,
                 headers: {
-                    "Access-Control-Allow-Origin": "*"
+                    "Access-Control-Allow-Origin": "*",
                 },
                 body: JSON.stringify(token),
             };
