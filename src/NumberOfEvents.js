f2394ecd64bb8cd2a73a89bb63288fd808885718
Orlando-Adrian Calciu <orlandoadrian5444@outlook.com>
2022-09-06 13:52:38 +0100

    Create ErrorAlert

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,18 +1,35 @@
 import React, { Component } from "react";
+import { ErrorAlert } from "./Alert";
 
 export class NumberOfEvents extends Component {
-    state = { numberOfEvents: 32 };
+    state = { 
+        numberOfEvents: 32,
+        infoText: '',
+    };
 
     handleInputChanged = (event) => {
         const value = event.target.value;
-        this.setState({ numberOfEvents: value });
+        if (value >= 33 || value <= 0) {
+            this.setState({
+                numberOfEvents: value,
+                infoText: 'Please enter a number between 1 - 32',
+            });
+        } else {
+            this.setState({
+                numberOfEvents: value,
+                infoText: '',
+            });
+        }
+
+        this.props.updateEvents(undefined, value);
     };
 
     render() {
         return (
             <div className="numberofEvents"> 
-            <br></br>
+            <ErrorAlert text={this.setState.infoText} />
             <label>Number of Events:</label>
+            <br></br>
                 <input 
                 type='number'
                 className="number-input"
