ae81450a3815eb5afe4a1192d8cc31835cc0999d
Orlando-Adrian Calciu <orlandoadrian5444@outlook.com>
2022-09-09 12:58:58 +0100

    Updates
    Adding privacy.html and WelcomeScreen

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { extractLocations, getEvents } from './api';
+import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
+import WelcomeScreen from './WelcomeScreen';
 import './nprogress.css';
 import { OfflineAlert } from './Alert';
 
@@ -15,16 +16,25 @@ class App extends Component {
         locations: [],
         numberOfEvents: 32,
         savedLocation: 'all',
+        showWelcomeScreen: undefined
     }
 
-    componentDidMount() {
+    async componentDidMount() {
         this.mounted = true;
-        getEvents().then((events) => {
-            if (this.mounted) {
-                this.setState({ events, locations: extractLocations(events) });
-            }
-        });
+        const accessToken = localStorage.getItem('access_token');
+        const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+        const searchParams = new URLSearchParams(window.location.search);
+        const code = searchParams.get("code");
+        this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+        if ((code || isTokenValid) && this.mounted) {
+            getEvents().then((events) => {
+                if (this.mounted) {
+                    this.setState({ events, locations: extractLocations(events) });
+                }
+            });
+        }
     }
+
 
     componentWillUnmount() {
         this.mounted = false;
@@ -49,8 +59,12 @@ class App extends Component {
     };
 
     render() {
+        if (this.state.showWelcomeScreen === undefined) return <div className='App' />
         return (
             <div className="App">
+                <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => {
+                    getAccessToken();
+                }} />
                 <div className='offlineAlert'>
                     {!navigator.onLine && (
                         <OfflineAlert text={"You are currently offline"} />
