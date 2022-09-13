b98e1cd528823cabaef9fed174b9a6406952b388
Orlando-Adrian Calciu <orlandoadrian5444@outlook.com>
2022-09-13 13:31:34 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -27,12 +27,12 @@ export const getEvents = async () => {
         return mockData;
     }
 
-    if (!navigator.onLine) {
-        console.log('You are offline!');
-        const data = localStorage.getItem("lastEvents");
-        NProgress.done();
-        return data ? JSON.parse(data).events : [];
-    }
+    // if (!navigator.onLine) {
+    //     console.log('You are offline!');
+    //     const data = localStorage.getItem("lastEvents");
+    //     NProgress.done();
+    //     return data ? JSON.parse(data).events : [];
+    // }
 
     const token = await getAccessToken();
 
