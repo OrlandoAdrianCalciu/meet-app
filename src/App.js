ea0966757470dcd14bcb7a20f798654922a9ae10
Orlando-Adrian Calciu <orlandoadrian5444@outlook.com>
2022-09-13 13:11:57 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,18 +22,24 @@ class App extends Component {
     async componentDidMount() {
         this.mounted = true;
         const accessToken = localStorage.getItem('access_token');
-        const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+        let isTokenValid;
+        if (accessToken && !navigator.onLine) {
+            isTokenValid = true;
+        } else {
+            isTokenValid = (await checkToken(accessToken)).error ? false : true;
+        }
         const searchParams = new URLSearchParams(window.location.search);
         const code = searchParams.get("code");
         this.setState({ showWelcomeScreen: !(code || isTokenValid) });
         if ((code || isTokenValid) && this.mounted) {
             getEvents().then((events) => {
                 if (this.mounted) {
-                    this.setState({ events, locations: extractLocations(events) });
+                    this.setState({ events: events.slice(0, this.state.numberOfEvents), locations: extractLocations(events) });
                 }
             });
         }
-    }
+    };
+
 
 
     componentWillUnmount() {
