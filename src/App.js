81b56fdf7fe70f0251ad865837ab89162efe3697
Orlando-Adrian Calciu <orlandoadrian5444@outlook.com>
2022-09-13 14:04:08 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NumberOfEvents from './NumberOfEvents';
 import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
 import WelcomeScreen from './WelcomeScreen';
 import './nprogress.css';
+import { OfflineAlert } from './Alert';
 // import { OfflineAlert } from './Alert';
 
 
@@ -19,31 +20,25 @@ class App extends Component {
         showWelcomeScreen: undefined
     }
 
-    componentDidMount() {
+    async componentDidMount() {
         this.mounted = true;
-        getEvents().then((events) => {
+        const accessToken = localStorage.getItem("access_token");
+        const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+        const searchParams = new URLSearchParams(window.location.search);
+        const code = searchParams.get("code");
+        this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+        if ((code || isTokenValid) && this.mounted) {
+          getEvents().then((events) => {
             if (this.mounted) {
-                this.setState({ events, locations: extractLocations(events) });
+              let sliceNumber = this.state.numberOfEvents;
+              this.setState({
+                locations: extractLocations(events),
+                events: events.slice(0, sliceNumber),
+              });
             }
-        });
-        //     const accessToken = localStorage.getItem('access_token');
-        //     let isTokenValid;
-        //     if (accessToken && !navigator.onLine) {
-        //         isTokenValid = true;
-        //     } else {
-        //         isTokenValid = (await checkToken(accessToken)).error ? false : true;
-        //     }
-        //     const searchParams = new URLSearchParams(window.location.search);
-        //     const code = searchParams.get("code");
-        //     this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-        //     if ((code || isTokenValid) && this.mounted) {
-        //         getEvents().then((events) => {
-        //             if (this.mounted) {
-        //                 this.setState({ events: events.slice(0, this.state.numberOfEvents), locations: extractLocations(events) });
-        //             }
-        //         });
-        //     }
-    };
+          });
+        }
+    }
 
 
 
@@ -68,6 +63,7 @@ class App extends Component {
             });
         });
     };
+    
 
     render() {
         if (this.state.showWelcomeScreen === undefined) return <div className='App' />
@@ -76,11 +72,11 @@ class App extends Component {
                 <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => {
                     getAccessToken();
                 }} />
-                {/* <div className='offlineAlert'>
+                <div className='offlineAlert'>
                     {!navigator.onLine && (
                         <OfflineAlert text={"You are currently offline"} />
                     )}
-                </div> */}
+                </div>
                 <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
                 <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents} />
                 <EventList events={this.state.events} />
