79242d75a15607b955ecca78fb5cb8f198789109
Orlando-Adrian Calciu <orlandoadrian5444@outlook.com>
2022-09-19 12:39:29 +0100

    Data Visualization

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
 import WelcomeScreen from './WelcomeScreen';
 import './nprogress.css';
 import { OfflineAlert } from './Alert';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 
 
 
@@ -20,7 +21,7 @@ class App extends Component {
         showWelcomeScreen: undefined
     }
 
-    async componentDidMount() { 
+    async componentDidMount() {
         this.mounted = true;
         const accessToken = localStorage.getItem("access_token");
         const isTokenValid = (await checkToken(accessToken)).error ? false : true;
@@ -28,15 +29,15 @@ class App extends Component {
         const code = searchParams.get("code");
         this.setState({ showWelcomeScreen: !(code || isTokenValid) });
         if ((code || isTokenValid) && this.mounted) {
-          getEvents().then((events) => {
-            if (this.mounted) {
-              let sliceNumber = this.state.numberOfEvents;
-              this.setState({
-                locations: extractLocations(events),
-                events: events.slice(0, sliceNumber),
-              });
-            }
-          });
+            getEvents().then((events) => {
+                if (this.mounted) {
+                    let sliceNumber = this.state.numberOfEvents;
+                    this.setState({
+                        locations: extractLocations(events),
+                        events: events.slice(0, sliceNumber),
+                    });
+                }
+            });
         }
     }
 
@@ -63,7 +64,17 @@ class App extends Component {
             });
         });
     };
-    
+
+    getData = () => {
+        const { locations, events } = this.state;
+        const data = locations.map((location) => {
+            const number = events.filter((event) => event.location === location).length
+            const city = location.split(', ').shift()
+            return { city, number };
+        })
+        return data;
+    };
+
 
     render() {
         if (this.state.showWelcomeScreen === undefined) return <div className='App' />
@@ -79,6 +90,19 @@ class App extends Component {
                 </div>
                 <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
                 <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents} />
+
+                <h4>Events in each city</h4>
+
+                <ScatterChart width={800} height={250}
+                    margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis type="category" dataKey="city" name="city" />
+                    <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
+                    <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                    <Scatter name="A school" data={this.getData()} fill="#8884d8" />
+                </ScatterChart>
+
+
                 <EventList events={this.state.events} />
             </div>
         );
